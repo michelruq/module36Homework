557e1a54ecaec375324aeb3bb962355befb1b58a
Mike Chashchin <michelruq@mail.ru>
2024-08-28 13:55:59 +0500

    Add mainwindow, application runs and completes

--- sources/Test.cpp
+++ sources/Test.cpp
@@ -2,11 +2,15 @@
 #include "CommandLineInterface.h"
 #include <QApplication>
 
+#include "mainwindow.h"
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
    QApplication a(argc, argv);
+   MainWindow w;
+   w.show();
    return a.exec();
 }
 
